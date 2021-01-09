2ca85ba0df7dca4c6ed28ad795d6ae05e374eb58
Kisha Martin <kisha.martin0727@gmail.com>
2021-01-09 13:25:27 -0600

    Week 2 Workshop Activity

---
+++ models/partner.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+    name: {
+        type: String, 
+        require: true, 
+        unique: true
+    }, 
+    image: {
+        type: String, 
+        require: true
+    }, 
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    description: {
+        type: String,
+        require: true
+    }, 
+},  {
+    timestamps: true
+});
+
+const Partner = mongoose.model('Partner', partnerSchema);
+
+module.exports = Partner;
